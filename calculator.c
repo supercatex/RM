8c6465f242263bfda102186f5d3b3f2a2300abdf
Kinda Lam <3446627+supercatex@users.noreply.github.com>
2019-06-04 17:35:46 +0800

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -1,48 +1,52 @@
 #include "dev.h"
 #include "tutorial_lib.h"
 
+// 獲取按鈕模塊的按鍵狀態
 extern key_sensor_t key_val[4];
 
+// 顯示整數在數碼管模塊上 ( 參數1: 硬件ID號, 參數2: 整數 )
 void show_num(uint8_t id, int num)
 {
-	int s = 0;
-	if (num < 0) s = 1;
+	int s = 0;				// 正負號：預設為 0.，即正數。
+	if (num < 0) s = 1;     		// 如果數字為負數，設為 1.
 
-	num = abs(num);
-	int n1 = num / 1000;
-	int n2 = num % 1000 / 100;
-	int n3 = num % 100 / 10;
-	int n4 = num % 10;
+	num = abs(num);         		// 絶對值數字
+	int n1 = num / 1000;			// 千位數
+	int n2 = num % 1000 / 100;		// 百位數
+	int n3 = num % 100 / 10;		// 十位數
+	int n4 = num % 10;			// 個位數
 
 	num_ctrl_t n;
-	n.pnt_4 = s;
+	n.pnt_4 = s;				// 四號小點亮代表負數，不亮代表正數。
 
-	n.enable_1 = (n1 != 0);
-	n.enable_2 = (n.enable_1 || n2 != 0);
-	n.enable_3 = (n.enable_2 || n3 != 0);
-	n.enable_4 = 1;
+	n.enable_1 = (n1 != 0);		        // 千位數不為零顯示千位
+	n.enable_2 = (n.enable_1 || n2 != 0);	// 千位數已亮或百位數不為零顯示百位
+	n.enable_3 = (n.enable_2 || n3 != 0);   // 百位數已亮或十位數不為零顯示十位
+	n.enable_4 = 1;                         // 顯示個位
 
-	n.num_1 = n1;
-	n.num_2 = n2;
-	n.num_3 = n3;
-	n.num_4 = n4;
+	n.num_1 = n1;                           // 設置千位數
+	n.num_2 = n2;				// 設置百位數
+	n.num_3 = n3;				// 設置十位數
+	n.num_4 = n4;				// 設置個位數
 	num_ctrl_data(id, n);
 }
 
+// 獲取按下的按鍵數字 ( 參數1: 按鈕數據 )
 int get_key(key_sensor_t data)
 {
-	if (data.key1 == 1) return 1;
-	if (data.key2 == 1) return 2;
-	if (data.key3 == 1) return 3;
-	if (data.key4 == 1) return 4;
-	if (data.key5 == 1) return 5;
-	if (data.key6 == 1) return 6;
-	if (data.key7 == 1) return 7;
-	if (data.key8 == 1) return 8;
-	if (data.key9 == 1) return 9;
-	return 0;
+	if (data.key1 == 1) return 1;		// 如果 Key 1 按下，則返回 1.
+	if (data.key2 == 1) return 2;		// 如果 Key 2 按下，則返回 2.
+	if (data.key3 == 1) return 3;		// 如果 Key 3 按下，則返回 3.
+	if (data.key4 == 1) return 4;		// 如果 Key 4 按下，則返回 4.
+	if (data.key5 == 1) return 5;		// 如果 Key 5 按下，則返回 5.
+	if (data.key6 == 1) return 6;		// 如果 Key 6 按下，則返回 6.
+	if (data.key7 == 1) return 7;		// 如果 Key 7 按下，則返回 7.
+	if (data.key8 == 1) return 8;		// 如果 Key 8 按下，則返回 8.
+	if (data.key9 == 1) return 9;		// 如果 Key 9 按下，則返回 9.
+	return 0;				// 如果都不是，則返回 0.
 }
 
+// 主程序
 void class_test_task(void const *argu)
 {
 	int is_pressed = 0;
